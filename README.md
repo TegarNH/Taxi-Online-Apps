1bc4eae82d3de8226b4c87f5f1be1857dd83a8a1
TegarNH <tegarnaufalh@gmail.com>
2023-03-13 12:02:47 +0700

    upload project

---
+++ README.md
@@ -0,0 +1,91 @@
+# Taxi Online Apps
+
+Simple Calculator Apps is an application created to perform simple calculations of two numbers. This calculator can calculate Addition, Subtraction, Multiplication and Division operations simultaneously. This program is designed with an attractive and simple appearance so that users can operate the program easily. This program is made using **HTML (Hypertext Markup Language)**, **CSS (Cascading Style Sheet)** and **PHP (Hypertext Preprocessor)** programming languages.
+
+<img src="https://res.cloudinary.com/dzskwtwm7/image/upload/v1677153229/1_blgqsu.png" width="500">
+<img src="https://res.cloudinary.com/dzskwtwm7/image/upload/v1677153229/2_ywbzgk.png" width="400">
+<br/>
+
+## Table of Contents
+
+- [Features](#features)
+- [Built with](#built-with)
+- [Project Structure](#project-structure)
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Local Usage](#local-usage)
+- [Author](#author)
+  <br/>
+
+## Features
+
+- Addition operation of two numbers.
+- Subtraction operation of two numbers.
+- Multiplication operation of two numbers.
+- Division operation of two numbers.
+- Calculation result is displayed with thousands separator.
+- Recalculate button to make it easier to do the calculation again.
+  <br/>
+
+## Built with
+
+| [HTML5](https://developer.mozilla.org/en-US/docs/Web/HTML)                                  | [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS)                                  | [PHP](https://www.php.net/)                                                              |
+| ------------------------------------------------------------------------------------------- | ----------------------------------------------------------------------------------------- | ---------------------------------------------------------------------------------------- |
+| <img src="https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg" width="100"> | <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg" width="100"> | <img src="https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg" width="100"> |
+
+<br/>
+
+## Project Structure
+
+Here's the folder structure for this project:
+
+```
+📦Taxi-Online-Apps
+ ┣ 📂css
+ ┃ ┗ 📜bootstrap.css
+ ┣ 📂data
+ ┃ ┗ 📜data.json
+ ┣ 📂img
+ ┃ ┗ 📜logo.jpg
+ ┣ 📜index.php
+ ┗ 📜README.md
+
+```
+
+## Requirements
+
+- Web Server Apps, XAMPP : PHP v8.0.0 and above.
+- Browser Applications, such as Microsoft Edge, Google Chrome, or Firefox.
+  <br/>
+
+## Installation
+
+Download [XAMPP](https://www.apachefriends.org/download.html) according to the operating system used, then install according to the instructions.
+<br/><br/>
+
+## Local Usage
+
+To get started running the project locally, please follow the steps below.
+
+1. Clone the repository
+
+```bash
+git clone https://github.com/TegarNH/Taxi-Online-Apps.git
+```
+
+2. Place the **Taxi-Online-Apps** folder into the
+   > _C:\xampp\htdocs_
+3. Open **XAMPP** applications and running **Apache** service.
+4. Open the Browser App and type
+   > _localhost/Taxi-Online-Apps/index.php_ > <br/>
+
+## Author
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/TegarNH"><img src="https://avatars.githubusercontent.com/u/55114771?v=4" width="100px;" alt=""/></a><br />
+      <a href="https://www.linkedin.com/in/tegarnaufalhanip/"><p style="font-size:10px">Tegar Naufal Hanip</p>
+    </td>
+  </tr>
+</table>
---
+++ index.php
@@ -0,0 +1,154 @@
+<?php
+
+	//	Instruksi Kerja Nomor 1.
+	//	Variabel $mobil berisi data jenis mobil yang dipesan dalam bentuk array.
+	$mobil = array("Avanza", "Rush", "Alphard", "Innova", "Fortuner");
+
+	//	Instruksi Kerja Nomor 2.
+	//	Mengurutkan array $mobil sesuai abjad A-Z.
+  sort($mobil);
+
+	//	Instruksi Kerja Nomor 4.
+	/* 
+    Fungsi untuk menghitung total tagihan perjalanan taxi online.
+    Fungsi ini menerima parameter berupa variabel jarak dan harga.
+    Fungsi ini akan mengembalikan nilai berupa perkalian antara variabel jarak dengan harga.
+  */
+	function total_tagihan($jarak, $harga){
+    return $jarak * $harga;
+	}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Pemesanan Taxi Online</title>
+		<!-- Instruksi Kerja Nomor 3. -->
+		<link rel="stylesheet" href="./css/bootstrap.css">
+	</head>
+	
+	<body>
+	<div class="container border">
+		<!-- Menampilkan judul halaman -->
+		<h3>Pemesanan Taxi Online</h3>
+		
+		<!-- Instruksi Kerja Nomor 7. -->
+		<!-- Menampilkan logo Taxi Online -->
+		<img src="./img/logo.jpg" alt="Logo Taxi Online">
+		
+		<!-- Form untuk memasukkan data pemesanan. -->
+		<form action="index.php" method="post" id="formPemesanan">
+			<div class="row">
+				<!-- Masukan data nama pelanggan. Tipe data text. -->
+				<div class="col-lg-2"><label for="nama">Nama Pelanggan:</label></div>
+				<div class="col-lg-2"><input type="text" id="nama" name="nama" required></div>
+			</div>
+			<div class="row">
+				<!-- Masukan data nomor HP pelanggan. Tipe data number. -->
+				<div class="col-lg-2"><label for="nomor">Nomor HP:</label></div>
+				<div class="col-lg-2"><input type="number" id="noHP" name="noHP" maxlength="16" required></div>
+			</div>
+			<div class="row">
+				<!-- Masukan pilihan jenis mobil. -->
+				<div class="col-lg-2"><label for="tipe">Jenis Mobil:</label></div>
+				<div class="col-lg-2">
+					<select id="mobil" name="mobil">
+					<option value="">- Jenis mobil -</option>
+					<?php
+						//	Instruksi Kerja Nomor 5.
+						//	Menampilkan dropdown pilihan jenis mobil Taxi Online berdasarkan data pada array $mobil menggunakan perulangan.
+            foreach ($mobil as $itemMobil) {
+              echo "<option value=$itemMobil>$itemMobil</option>";
+            }
+          ?>	
+					</select>
+				</div>
+			</div>
+			
+			<div class="row">
+				<!-- Masukan data Jarak Tempuh. Tipe data number. -->
+				<div class="col-lg-2"><label for="nomor">Jarak:</label></div>
+				<div class="col-lg-2"><input type="number" id="jumlahPesanan" name="jarak" maxlength="4" required></div>
+			</div>
+			<div class="row">
+				<!-- Tombol Submit -->
+				<div class="col-lg-2"><button class="btn btn-primary" type="submit" form="formPemesanan" value="Pesan" name="Pesan">Pesan</button></div>
+				<div class="col-lg-2"></div>		
+			</div>
+		</form>
+	</div>
+	<?php
+		//	Kode berikut dieksekusi setelah tombol Hitung ditekan.
+		if(isset($_POST['Pesan'])) {
+			
+			//	Variabel $dataPesanan berisi data-data pemesanan dari form dalam bentuk array.
+			
+			$dataPesanan = array(
+				'nama' => $_POST['nama'],
+				'noHP' => $_POST['noHP'],
+				'mobil' => $_POST['mobil'],
+				'jarak' => $_POST['jarak']
+			);
+			$jarak = $_POST['jarak'];
+
+      $tagihan = 0;
+			// Instruksi Kerja Nomor 6 (Percabangan)
+			if ($jarak <= 10) { // jika jarak kurang dari atau sama dengan 10 km
+        $tagihan = $jarak * 1000; 
+      } else { // jika jarak lebih dari 10 km
+        $tagihan = 10 * 1000; // harga 10 km pertama sebesar Rp 1.000/km
+        $tagihan += ($jarak - 10) * 5000; // km selanjutnya sebesar Rp 5.000/km
+      }
+      $dataPesanan["tagihan"] = $tagihan;
+
+			//	Variabel berisi path file data.json yang digunakan untuk menyimpan data pemesanan.
+			$berkas = "./data/data.json";
+			
+			//	Mengubah data pemesanan yang berbentuk array PHP menjadi bentuk JSON.
+			$dataJson = json_encode($dataPesanan, JSON_PRETTY_PRINT);
+			
+			//	Instruksi Kerja Nomor 8.
+			//	Menyimpan data pemesanan yang berbentuk JSON ke dalam file JSON
+			file_put_contents($berkas, $dataJson);
+			$dataJson = file_get_contents($berkas);
+			
+			//	Mengubah data pemesanan dalam format JSON ke dalam format array PHP.
+			$dataPesanan = json_decode($dataJson, true);
+
+			//	Menampilkan data pemesanan dan hasil perhitungan diskon dan tagihan.
+			echo "
+				<br/>
+				<div class='container'>
+					
+					<div class='row'>
+						<!-- Menampilkan nama pelanggan. -->
+						<div class='col-lg-2'>Nama Pelanggan:</div>
+						<div class='col-lg-2'>".$dataPesanan['nama']."</div>
+					</div>
+					<div class='row'>
+						<!-- Menampilkan nomor HP pelanggan. -->
+						<div class='col-lg-2'>Nomor HP:</div>
+						<div class='col-lg-2'>".$dataPesanan['noHP']."</div>
+					</div>
+					<div class='row'>
+						<!-- Menampilkan Jenis mobil Taxi Online. -->
+						<div class='col-lg-2'>Jenis Mobil:</div>
+						<div class='col-lg-2'>".$dataPesanan['mobil']."</div>
+					</div>
+					<div class='row'>
+						<!-- Menampilkan jumlah Jarak Tempuh. -->
+						<div class='col-lg-2'>Jarak(km):</div>
+						<div class='col-lg-2'>".$dataPesanan['jarak']." km</div>
+					</div>
+					<div class='row'>
+						<!-- Menampilkan Total Tagihan. -->
+						<div class='col-lg-2'>Total:</div>
+						<div class='col-lg-2'>Rp".number_format($tagihan, 0, ".", ".").",-</div>
+					</div>
+					
+			</div>
+			";
+		}
+	?>
+	</body>
+</html>
